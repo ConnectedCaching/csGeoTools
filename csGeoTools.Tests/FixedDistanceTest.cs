ea5db5e5bf71a59026c26ed616a28b2c2e03b43e
Manuel Zedel <slipknote@gmx.de>
2012-11-11 17:07:27 +0100

    fixeddistance tests added

---
+++ csGeoTools.Tests/FixedDistanceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace csGeoTools.Tests
+{
+    [TestClass]
+    public class FixedDistanceTest
+    {
+        [TestMethod]
+        public void FixedDistance_ReturnsInput_GivenMeters()
+        {
+            FixedDistance distance = new FixedDistance(12367);
+            Assert.AreEqual(12367, distance.In(MetricUnit.meters));
+        }
+
+        [TestMethod]
+        public void Conversions_ReturnSaneValues()
+        {
+            FixedDistance distance = new FixedDistance(10000);
+            Assert.AreEqual(10.0, distance.In(MetricUnit.kilometers));
+        }
+
+        [TestMethod]
+        public void Conversion_WorksWithImperialUnits()
+        {
+            FixedDistance distance = new FixedDistance(10000);
+            Assert.AreEqual(32808, Math.Round(distance.In(ImperialUnit.feet)));
+        }
+    }
+}
